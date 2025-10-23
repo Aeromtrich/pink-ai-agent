6e4fd975f002015fcd0b2c62b9d4c8bac8e88c60
Aeromtrich <aeromtrich@gmail.com>
2025-10-23 23:53:58 +0800

    项目初始化

---
+++ src/main/java/com/google/pinkaiagent/PinkAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.google.pinkaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinkAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PinkAiAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/google/pinkaiagent/PinkAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.google.pinkaiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinkAiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
